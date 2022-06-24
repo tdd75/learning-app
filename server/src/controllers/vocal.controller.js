22de95e80ab408c270692407f18b724e63b733dd
Phan Thanh Long <driverofl01@gmail.com>
2022-06-24 17:07:44 +0700

    BE-vol: rename service

--- server/src/controllers/vocal.controller.js
+++ server/src/controllers/vocal.controller.js
@@ -1,6 +1,8 @@
 import { httpStatus, apiStatus } from '../constants/index.js';
 import CustomError from '../error/custom.error.js';
-import UserService from '../service/word.service.js';
+import WordService from '../service/word.service.js';
+import UserService from '../service/user.service.js';
+
 
 /**
  * Get All Volcabulary (has paging)
@@ -13,8 +15,8 @@ export const getAllVol = async (req, res) => {
     let limit = req.query.limit;
     let offset = req.query.offset;
 
-    let word = await UserService.findWordPaging(limit, offset);
-    let totalPage = await UserService.countTotalPage(limit);
+    let word = await WordService.findWordPaging(limit, offset);
+    let totalPage = await WordService.countTotalPage(limit);
 
     let dataReturn = {
       "words": word,
@@ -44,27 +46,30 @@ export const getAllVol = async (req, res) => {
  * Get Process list vol for USER role
  * @param {Header: Authorization} req
  * @param {Object} res response API
- * @returns status API + message + user profile(if not error)
+ * @returns status API + message + page index process (600 word -> /10 topic. 60 word by topic)
  */
 export const getVolProcess = async (req, res) => {
-  // try {
-  //   let userId = req.userId;
-  //   let user = await UserService.findUserById(userId);
-  //   return res.status(httpStatus.OK).send({
-  //     status: apiStatus.SUCCESS,
-  //     message: 'get user information successfully!',
-  //     data: user,
-  //   });
-  // } catch (err) {
-  //   if (err instanceof CustomError) {
-  //     return res.status(err.httpStatus).send({
-  //       status: err.apiStatus,
-  //       message: err.message,
-  //     });
-  //   }
+  try {
+    let userId = req.userId;
+    let user = await UserService.findUserById(userId);
+
+    console.log(user.getVolProcess)
+
+    return res.status(httpStatus.OK).send({
+      status: apiStatus.SUCCESS,
+      message: 'get user information successfully!',
+      data: user,
+    });
+  } catch (err) {
+    if (err instanceof CustomError) {
+      return res.status(err.httpStatus).send({
+        status: err.apiStatus,
+        message: err.message,
+      });
+    }
   return res.status(httpStatus.INTERNAL_SERVER_ERROR).send({
     status: apiStatus.OTHER_ERROR,
     // message: err.message,
   });
-  // }
+  }
 };
